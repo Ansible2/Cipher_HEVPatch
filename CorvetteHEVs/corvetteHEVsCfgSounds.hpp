6bdbf6a5e4380745032dcf98073b6992da965213
Ansible2 <55198007+Ansible2@users.noreply.github.com>
2020-08-19 00:49:30 -0600

    Create corvetteHEVsCfgSounds.hpp

---
+++ CorvetteHEVs/corvetteHEVsCfgSounds.hpp
@@ -0,0 +1,65 @@
+class OPTRE_corvetteHEV_engineDropLoop
+{
+	dlc = "HEV Patch";
+	name = "[OPTRE] Corvette HEV Drop Engine Loop";
+	sound[]=
+	{
+		"Cipher_HEVPatch\Sounds\Corvette HEV\OPTRE_corvetteHEV_engineDropLoop.ogg",
+		1,
+		1
+	};
+	author = "Cipher // Ansible2";
+	titles[] = {};
+};
+class OPTRE_corvetteHEV_engineEWOStart
+{
+	dlc = "HEV Patch";
+	name = "[OPTRE] Corvette HEV EWO Start";
+	sound[]=
+	{
+		"Cipher_HEVPatch\Sounds\Corvette HEV\OPTRE_corvetteHEV_engineEWOStart.ogg",
+		1,
+		1
+	};
+	author = "Cipher // Ansible2";
+	titles[] = {};
+};
+class OPTRE_corvetteHEV_engineIdleLoop
+{
+	dlc = "HEV Patch";
+	name = "[OPTRE] Corvette HEV EWO Start";
+	sound[]=
+	{
+		"Cipher_HEVPatch\Sounds\Corvette HEV\OPTRE_corvetteHEV_engineIdleLoop.ogg",
+		1,
+		1
+	};
+	author = "Cipher // Ansible2";
+	titles[] = {};
+};
+class OPTRE_corvetteHEV_engineNormalStart
+{
+	dlc = "HEV Patch";
+	name = "[OPTRE] Corvette HEV Normal Start";
+	sound[]=
+	{
+		"Cipher_HEVPatch\Sounds\Corvette HEV\OPTRE_corvetteHEV_engineNormalStart.ogg",
+		1,
+		1
+	};
+	author = "Cipher // Ansible2";
+	titles[] = {};
+};
+class OPTRE_corvetteHEV_engineShutdown
+{
+	dlc = "HEV Patch";
+	name = "[OPTRE] Corvette HEV Engine Shutdown";
+	sound[]=
+	{
+		"Cipher_HEVPatch\Sounds\Corvette HEV\OPTRE_corvetteHEV_engineShutdown2.ogg",
+		1,
+		1
+	};
+	author = "Cipher // Ansible2";
+	titles[] = {};
+};
